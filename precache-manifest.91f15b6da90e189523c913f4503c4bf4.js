7b57c3b92deb52a75b0816dfba95e20de555f590
Marlo Dela Torre <mato@splitmedialabs.com>
2021-01-24 10:29:14 +0800

    initial release

---
+++ precache-manifest.91f15b6da90e189523c913f4503c4bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcd557b4c216b964f0e9bc9f2b89800",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c063a374b7aa9a62dca8",
+    "url": "./static/css/main.435f2802.chunk.css"
+  },
+  {
+    "revision": "8d717d02d3e9e1a6e001",
+    "url": "./static/js/2.55b89219.chunk.js"
+  },
+  {
+    "revision": "2a7f2b06627b12300df476abbbc38880",
+    "url": "./static/js/2.55b89219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c063a374b7aa9a62dca8",
+    "url": "./static/js/main.7a91ae32.chunk.js"
+  },
+  {
+    "revision": "191293f5ba5c7cf4fc68",
+    "url": "./static/js/runtime-main.54ab1ece.js"
+  }
+]);
